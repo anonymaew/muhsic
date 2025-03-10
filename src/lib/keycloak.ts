d7f50d0981bdb1b214c6d1fe9b0885b56adc0229
Napat Srichan <mail@napatsc.com>
2025-03-10 01:18:49 -0700

    lib cleanup

--- src/lib/keycloak.ts
+++
@@ -1,6 +0,0 @@
-const KEYCLOAK_HOST = 'https://muhsic.acad.ucsc.edu/auth'
-
-export readAttributes = async (username) => {
-  await fetch(`https://${KEYCLOAK_HOST}/auth/realms/master/protocol/openid-connect/token`);
-
-}
--- src/lib/test.sh
+++
@@ -1,1 +0,0 @@
-curl https://muhsic.acad.ucsc.edu/table/dltemp/l-4WVDKOBSA4-zjq/1738847400000/noco/pocuuhg9xrekfww/mg8gd1drtnn6c84/c429uqr7t417xzy/UCSC_AS_1_ENGLISH_ZzE2j.pdf -O
